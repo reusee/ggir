73d57d340091bc67d768e8f1e30af911dd252947
reus <reusee@gmail.com>
2014-04-29 12:40:54 +0800

    generator: code cleanup

--- function.go
+++ function.go
@@ -1,21 +1,65 @@
 package main
 
 import (
+	"bytes"
+	"go/format"
+	"io"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 )
 
+func (self *Generator) GenFunctions(ns *Namespace) {
+	output := new(bytes.Buffer)
+	w(output, "package %s\n\n", self.PackageName)
+	w(output, "/*\n")
+	for _, include := range self.Includes {
+		w(output, "#include <%s>\n", include)
+	}
+	if len(self.PkgConfigs) > 0 {
+		w(output, "#cgo pkg-config: %s\n", strings.Join(self.PkgConfigs, " "))
+	}
+	w(output, "*/\n")
+	w(output, "import \"C\"\n\n")
+	w(output, `import ("unsafe";	"reflect"; "errors")
+func init() {
+	var _ unsafe.Pointer
+	var _ reflect.SliceHeader
+	_ = errors.New("")
+}
+`)
+	for _, fn := range ns.Functions {
+		self.GenFunction(fn, output)
+	}
+	for typeSpec, funcs := range typeStat {
+		p("===fixme=== %s TYPE NOT MAPPED => %s\n", self.PackageName, typeSpec)
+		p("%s\n\n", strings.Join(funcs, "\n"))
+	}
+	f, err := os.Create(filepath.Join(self.outputDir, self.PackageName+"_functions.go"))
+	checkError(err)
+	formatted, err := format.Source(output.Bytes())
+	if err != nil {
+		f.Write(output.Bytes())
+		f.Close()
+		p("==> %s\n", self.GirPath)
+		checkError(err)
+	}
+	f.Write(formatted)
+	f.Close()
+}
+
 var typeStat = make(map[string][]string)
 
-func (self *Generator) GenFunction(fn *Function) {
+func (self *Generator) GenFunction(fn *Function, output io.Writer) {
 	// skip deprecated
 	if fn.Deprecated != "" {
-		w(self.funcsOutput, "// %s is not generated due to deprecation attr\n\n", fn.CIdentifier)
+		w(output, "// %s is not generated due to deprecation attr\n\n", fn.CIdentifier)
 		return
 	}
 	for _, f := range self.FunctionDeprecated {
 		if fn.CIdentifier == f {
-			w(self.funcsOutput, "// %s is not generated due to explicit deprecation\n\n", fn.CIdentifier)
+			w(output, "// %s is not generated due to explicit deprecation\n\n", fn.CIdentifier)
 			return
 		}
 	}
@@ -25,7 +69,7 @@ func (self *Generator) GenFunction(fn *Function) {
 		matched, err := regexp.MatchString(pattern, fn.CIdentifier)
 		checkError(err)
 		if matched {
-			w(self.funcsOutput, "// %s is not generated due to explicit ignore\n\n", fn.CIdentifier)
+			w(output, "// %s is not generated due to explicit ignore\n\n", fn.CIdentifier)
 			return
 		}
 	}
@@ -35,14 +79,14 @@ func (self *Generator) GenFunction(fn *Function) {
 
 	// skip varargs functions
 	if fn.IsVarargs {
-		w(self.funcsOutput, "// %s is not generated due to varargs\n\n", fn.CIdentifier)
+		w(output, "// %s is not generated due to varargs\n\n", fn.CIdentifier)
 		return
 	}
 
 	//FIXME skip function with inout param due to broken rule
 	for _, param := range fn.Params {
 		if param.Direction == "inout" {
-			w(self.funcsOutput, "// %s is not generated due to inout param\n\n", fn.CIdentifier)
+			w(output, "// %s is not generated due to inout param\n\n", fn.CIdentifier)
 			return
 		}
 	}
@@ -53,7 +97,7 @@ func (self *Generator) GenFunction(fn *Function) {
 
 		//FIXME skip functions with long double param
 		if param.CType == "long double" {
-			w(self.funcsOutput, "// %s is not generated due to long double param\n\n", fn.CIdentifier)
+			w(output, "// %s is not generated due to long double param\n\n", fn.CIdentifier)
 			return
 		}
 
@@ -71,101 +115,101 @@ func (self *Generator) GenFunction(fn *Function) {
 
 	// generate doc
 	if fn.Doc != nil {
-		w(self.funcsOutput, "/*\n%s\n*/\n", fn.Doc.Text)
+		w(output, "/*\n%s\n*/\n", fn.Doc.Text)
 	}
 
 	// generate signature
-	w(self.funcsOutput, "func ")
+	w(output, "func ")
 
 	// generate receiver FIXME
 
 	// generate function name
-	w(self.funcsOutput, fn.GoName)
+	w(output, fn.GoName)
 
 	// generate parameters
-	w(self.funcsOutput, "(")
+	w(output, "(")
 	for _, param := range fn.Params {
 		if !param.IsVoid && param.Direction == "in" {
-			w(self.funcsOutput, "%s %s, ", param.GoName, param.MappedType)
+			w(output, "%s %s, ", param.GoName, param.MappedType)
 		}
 	}
-	w(self.funcsOutput, ")")
+	w(output, ")")
 
 	// generate returns
-	w(self.funcsOutput, "(")
+	w(output, "(")
 	for _, param := range fn.Params {
 		if !param.IsVoid && param.Direction == "out" {
-			w(self.funcsOutput, "%s %s, ", param.GoName, param.MappedType)
+			w(output, "%s %s, ", param.GoName, param.MappedType)
 		}
 	}
 	if !fn.Return.IsVoid { // return
-		w(self.funcsOutput, "%s %s, ", fn.Return.GoName, fn.Return.MappedType)
+		w(output, "%s %s, ", fn.Return.GoName, fn.Return.MappedType)
 	}
 	if fn.Throws != "" { // error
-		w(self.funcsOutput, "__err__ error")
+		w(output, "__err__ error")
 	}
-	w(self.funcsOutput, ")")
+	w(output, ")")
 
 	// body start
-	w(self.funcsOutput, "{\n")
+	w(output, "{\n")
 
 	// statements before cgo call
 	for _, param := range fn.Params {
 		if param.CgoBeforeStmt != "" {
-			w(self.funcsOutput, "%s\n", param.CgoBeforeStmt)
+			w(output, "%s\n", param.CgoBeforeStmt)
 		}
 	}
 
 	// error param
 	if fn.Throws != "" {
-		w(self.funcsOutput, "var __cgo_error__ *C.GError\n")
+		w(output, "var __cgo_error__ *C.GError\n")
 	}
 
 	// cgo return value
 	if !fn.Return.IsVoid {
 		if fn.Return.GoType != fn.Return.MappedType { // mapped
-			w(self.funcsOutput, "var __cgo__return__ %s\n", fn.Return.GoType)
-			w(self.funcsOutput, "__cgo__return__ = ")
+			w(output, "var __cgo__return__ %s\n", fn.Return.GoType)
+			w(output, "__cgo__return__ = ")
 		} else { // not mapped
-			w(self.funcsOutput, "%s = ", fn.Return.GoName)
+			w(output, "%s = ", fn.Return.GoName)
 		}
 	}
 
 	// cgo call
-	w(self.funcsOutput, "C.%s(", fn.CIdentifier)
+	w(output, "C.%s(", fn.CIdentifier)
 	for _, param := range fn.Params {
 		if !param.IsVoid {
-			w(self.funcsOutput, "%s,", param.CgoParam)
+			w(output, "%s,", param.CgoParam)
 		}
 	}
 	if fn.Throws != "" { // error param
-		w(self.funcsOutput, "&__cgo_error__")
+		w(output, "&__cgo_error__")
 	}
-	w(self.funcsOutput, ")\n")
+	w(output, ")\n")
 
 	// statements after cgo call
 	for _, param := range fn.Params {
 		if param.CgoAfterStmt != "" {
-			w(self.funcsOutput, "%s\n", param.CgoAfterStmt)
+			w(output, "%s\n", param.CgoAfterStmt)
 		}
 	}
 	if fn.Return.CgoAfterStmt != "" {
-		w(self.funcsOutput, "%s\n", fn.Return.CgoAfterStmt)
+		w(output, "%s\n", fn.Return.CgoAfterStmt)
 	}
 
 	// convert GError to error
 	if fn.Throws != "" {
-		w(self.funcsOutput, `if __cgo_error__ != nil {
+		w(output, `if __cgo_error__ != nil {
 	__err__ = errors.New(C.GoString((*C.char)(unsafe.Pointer(__cgo_error__.message))))
 }
 `)
 	}
 
 	// body end
-	w(self.funcsOutput, "return\n}\n")
+	w(output, "return\n}\n")
 
 	// blank line
-	w(self.funcsOutput, "\n")
+	w(output, "\n")
 }
 
 func (self *Function) CollectInfo() {
--- generator.go
+++ generator.go
@@ -1,19 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"encoding/xml"
-	"go/format"
-	"io"
 	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
 )
 
 type Generator struct {
-	funcsOutput io.Writer
-	outputDir   string
+	outputDir string
 
 	PackageName            string   `xml:"package-name"`
 	GirPath                string   `xml:"gir-path"`
@@ -44,43 +37,7 @@ func Gen(outputDir, buildFilePath string) {
 	ns := repo.Namespace
 
 	// functions
-	funcsOutput := new(bytes.Buffer)
-	generator.funcsOutput = funcsOutput
-	w(funcsOutput, "package %s\n\n", generator.PackageName)
-	w(funcsOutput, "/*\n")
-	for _, include := range generator.Includes {
-		w(funcsOutput, "#include <%s>\n", include)
-	}
-	if len(generator.PkgConfigs) > 0 {
-		w(funcsOutput, "#cgo pkg-config: %s\n", strings.Join(generator.PkgConfigs, " "))
-	}
-	w(funcsOutput, "*/\n")
-	w(funcsOutput, "import \"C\"\n\n")
-	w(funcsOutput, `import ("unsafe";	"reflect"; "errors")
-func init() {
-	var _ unsafe.Pointer
-	var _ reflect.SliceHeader
-	_ = errors.New("")
-}
-`)
-	for _, fn := range ns.Functions {
-		generator.GenFunction(fn)
-	}
-	for typeSpec, funcs := range typeStat {
-		p("===fixme=== %s TYPE NOT MAPPED => %s\n", generator.PackageName, typeSpec)
-		p("%s\n\n", strings.Join(funcs, "\n"))
-	}
-	f, err := os.Create(filepath.Join(outputDir, generator.PackageName+"_functions.go"))
-	checkError(err)
-	formatted, err := format.Source(funcsOutput.Bytes())
-	if err != nil {
-		f.Write(funcsOutput.Bytes())
-		f.Close()
-		p("==> %s\n", generator.GirPath)
-		checkError(err)
-	}
-	f.Write(formatted)
-	f.Close()
+	generator.GenFunctions(ns)
 
 	// constants
 	generator.GenConstants(ns)
