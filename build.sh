7595e5da3ffcb74422b3e693de7f7bae6ba61d88
reus <reusee@gmail.com>
2014-05-05 11:14:53 +0800

    build.sh: add go get commands

--- build.sh
+++ build.sh
@@ -2,7 +2,16 @@
 go build
 ./ggir glib
 ./ggir gobject
+./ggir gio
 ./ggir gdk
 ./ggir gdkpixbuf
 ./ggir cairo
 ./ggir gtk
+echo 'go get'
+go get -u github.com/reusee/ggir/glib
+go get -u github.com/reusee/ggir/gobject
+go get -u github.com/reusee/ggir/gio
+go get -u github.com/reusee/ggir/gdk
+go get -u github.com/reusee/ggir/gdkpixbuf
+go get -u github.com/reusee/ggir/cairo
+go get -u github.com/reusee/ggir/gtk
