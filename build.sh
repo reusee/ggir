dc58d7455d45a19444b276bb2e0ac8dbebf67e78
reus <reusee@gmail.com>
2014-05-22 18:24:26 +0800

    do not run go get in build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,3 @@ go build
 ./ggir gdkpixbuf
 ./ggir cairo
 ./ggir gtk
-echo 'go get'
-go get -u github.com/reusee/ggir/glib
-go get -u github.com/reusee/ggir/gobject
-go get -u github.com/reusee/ggir/gio
-go get -u github.com/reusee/ggir/gdk
-go get -u github.com/reusee/ggir/gdkpixbuf
-go get -u github.com/reusee/ggir/cairo
-go get -u github.com/reusee/ggir/gtk
